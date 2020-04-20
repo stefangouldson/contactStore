1486a04c515bc1f85246d626847314ae6cd11c71
Stefan Gouldson <stefangouldson@gmail.com>
2020-04-20 14:07:37 +0100

    finished project

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+let axios = require('axios');
+
+Vue.component('contacts', require('./components/Contacts.vue').default);
+
+const app = new Vue({
+    el: '#app',
+})
---
+++ resources/js/components/Contacts.vue
@@ -0,0 +1,131 @@
+<template>
+    <div>
+        <h1>Add Contacts</h1>
+
+        <form action="" @submit.prevent='edit ? updateContact(contact.id) : createContact()'>
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input v-model="contact.name" type="text" name="name" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input v-model="contact.email" type="text" name="email" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="phone">Phone</label>
+                <input v-model="contact.phone" type="text" name="phone" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <button v-show="!edit" type="submit" class="btn btn-primary">Submit</button>
+                <button v-show="edit" type="submit" class="btn btn-primary">Update</button>
+            </div>
+        </form>
+
+        <h1>Contacts</h1>
+
+        <ul class="list-group">
+            <li v-for="contact in list" class="list-group-item">
+                <strong>{{ contact.name }}</strong> {{ contact.email }} {{ contact.phone }}
+                <div class="btn-group float-right">
+                    <button @click='showContact(contact.id)' class="btn btn-info btn-sm">Edit</button>
+                    <button @click='deleteContact(contact.id)' class="btn btn-danger btn-sm">Delete</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            edit:false,
+            list: [],
+            contact: {
+                id:'',
+                name: '',
+                email:'',
+                phone: ''
+            }
+        }
+    },
+    mounted: function(){
+        console.log('Contacts components loaded..')
+        this.fetchContactList();
+    },
+    methods:{
+        fetchContactList: function(){
+            console.log('fetching contacts');
+            axios.get('api/contacts')
+            .then((response) => {
+                console.log(response.data)
+                this.list = response.data
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        },
+
+        createContact: function(){
+            console.log('Creating contact...')
+            let self = this;
+            let params = Object.assign({}, self.contact);
+
+            axios.post('api/contact/store',params)
+                .then(function(){
+                    self.contact.name = '';
+                    self.contact.email = '';
+                    self.contact.phone = '';
+                    self.edit= false;
+                    self.fetchContactList();
+                })
+                .catch(function(error){
+                    console.log(error)
+                })
+        },
+        showContact: function(id){
+            let self = this;
+
+            axios.get('api/contact/'+id)
+            .then(function(response){
+                self.contact.id = response.data.id;
+                self.contact.name = response.data.name;
+                self.contact.email = response.data.email;
+                self.contact.phone = response.data.phone;
+            })
+            self.edit = true
+        },
+        updateContact: function(id){
+            console.log('Updating Contact ' + id)
+            let self = this;
+            let params = Object.assign({}, self.contact);
+
+            axios.patch('api/contact/'+id,params)
+                .then(function(){
+                    self.contact.name = '';
+                    self.contact.email = '';
+                    self.contact.phone = '';
+                    self.edit= false;
+                    self.fetchContactList();
+                })
+                .catch(function(error){
+                    console.log(error)
+                })
+        },
+        deleteContact: function(id) {
+            console.log('Deleting Contact' + id)
+            let self = this;
+            axios.delete('api/contact/'+id)
+            .then(function(response){
+                self.fetchContactList();
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+        }
+    }
+}
+</script>
